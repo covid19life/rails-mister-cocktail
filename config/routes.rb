50b643acb1db68b85d24d57d125f32eda6dea8dc
Francesca Hall <francescahall88@gmail.com>
2020-08-13 16:58:12 -0400

    completed day 1

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get 'cocktails', to: 'cocktails#index'
-
-  get 'cocktails/new', to: 'cocktails#new', as: :new_cocktail
-  post 'cocktails', to: 'cocktails#create'
-
-  get 'cocktails/:id', to: 'cocktails#show', as: :cocktail
-
-  get 'doses', to: 'doses#index'
-  get 'doses/new', to: 'doses#new', as: :new_dose
-
-  delete 'doses/:id', to: 'doses#destroy'
-
+  resources :cocktails, only: [:index, :show, :new, :create] do
+    resources :doses, only: [:new, :create]
+  end
+  resources :doses, only: [:destroy]
 end
